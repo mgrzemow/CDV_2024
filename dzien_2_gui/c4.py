b643c119d28a5bf094df31ce29ecd5f44de0c3d2
Michał Grzemowski <michal@grzmow.ski>
2024-04-28 14:07:08 +0200

    c4

--- dzien_2_gui/c4.py
+++ dzien_2_gui/c4.py
@@ -9,6 +9,38 @@
 import requests
 r = requests.get('https://api.nbp.pl/api/exchangerates/tables/a/?format=json')
 j = r.json()
-print(j[0]['rates'][0]['mid'])
+kursy = {d['code']: d['mid'] for d in j[0]['rates']}
+# kursy = {}
+# for d in j[0]['rates']:
+#     kursy[d['code']] = d['mid']
 
+import tkinter as tk
+from tkinter import ttk
+
+
+if __name__ == '__main__':
+    try:
+        from ctypes import windll
+
+        windll.shcore.SetProcessDpiAwareness(1)
+    except:
+        ...
+    root = tk.Tk()
+    # szerokoscxwysokosc+x_offset+y_offset
+    root.geometry('700x400+5000+1000')
+    root.title('Kursy walut')
+    e1 = ttk.Entry()
+    c1 = ttk.Combobox(values=list(kursy.keys()))
+    b1 = ttk.Button(root, text='Wylicz kurs')
+    label = ttk.Label(root, text='')
+    def f1(event):
+        print(event)
+
+    b1.bind('<Button>', f1)
+    # b1.focus()
+    e1.pack()
+    c1.pack()
+    b1.pack()
+    label.pack()
+    root.mainloop()
 
