b1348d0737c67e89077ce0e8fa1dbe5bec51e3bd
Michał Grzemowski <michal@grzmow.ski>
2024-06-08 11:37:49 +0200

    wzorce 2

--- dzien_6_wzorce/c1_factory.py
+++ dzien_6_wzorce/c1_factory.py
@@ -30,17 +30,53 @@ class Pracownik:
     def __str__(self):
         return f'{self.__class__.__name__}("{self._imie}")'
 
+
 class Kierownik(Pracownik):
     # dodatkowa premia, kwota za pracę powyżejh 10h
+    __slots__ = [
+        'bonus_kier',
+    ]
+
+    def __init__(self, imie, stawka_norm, stawka_nad, bonus_kier):
+        ...
+        # zapisać w self wartości parametrów
+        # doczytac o mandatory keywork only parameters
+        super().__init__(imie, stawka_norm, stawka_nad)
+        self.bonus_kier = bonus_kier
+
     def pracuj(self, ile_h):
         # wyliczyć zarobki i zwiększyć licznik
         super().pracuj(ile_h)
         if ile_h >= 10:
             self._zarobki += self.bonus_kier
 
-p1 = Kierownik('Jan', 30, 60, 300)
-p1.pracuj(2)  # naliczy się 2*30 czyli 60
-p1.pracuj(10)  # naliczy się 8*30 + 2 * 60 + 300 czyli 660
-print(p1)  # Pracownik("Jan")
-print(p1.wyplata())  # 720
-print(p1.wyplata())  # 0
+
+class Dyrektor(Kierownik):
+    # dodatkowa premia, kwota za pracę powyżejh 10h
+    __slots__ = [
+        'bonus_dyr',
+    ]
+
+    def __init__(self, imie, stawka_norm, stawka_nad, bonus_kier, bonus_dyr):
+        ...
+        # zapisać w self wartości parametrów
+        # doczytac o mandatory keywork only parameters
+        super().__init__(imie, stawka_norm, stawka_nad, bonus_kier)
+        self.bonus_dyr = bonus_dyr
+
+    def pracuj(self, ile_h):
+        # wyliczyć zarobki i zwiększyć licznik
+        super().pracuj(ile_h)
+        self._zarobki += self.bonus_dyr
+
+
+def zatrudnij_zepol(ile_p, ile_k, ile_d):
+    lista_p = []
+    ...  # dodajemy odpowiednie obiekty do listy, imiona i stawki dowolne
+    return lista_p
+
+
+if __name__ == '__main__':
+    zespol = zatrudnij_zepol(10, 2, 1)
+    # wszyscy pracują 2, 9, 11 h
+    # policzyć sumę wypłat
