83bc24a30c522bd79229638612fad8b1b63077c0
Michał Grzemowski <michal@grzmow.ski>
2024-06-08 11:16:49 +0200

    wzorce 2

--- dzien_6_wzorce/c1_factory.py
+++ dzien_6_wzorce/c1_factory.py
@@ -30,11 +30,17 @@ class Pracownik:
     def __str__(self):
         return f'{self.__class__.__name__}("{self._imie}")'
 
+class Kierownik(Pracownik):
+    # dodatkowa premia, kwota za pracę powyżejh 10h
+    def pracuj(self, ile_h):
+        # wyliczyć zarobki i zwiększyć licznik
+        super().pracuj(ile_h)
+        if ile_h >= 10:
+            self._zarobki += self.bonus_kier
 
-p1 = Pracownik('Jan', 30, 60)
+p1 = Kierownik('Jan', 30, 60, 300)
 p1.pracuj(2)  # naliczy się 2*30 czyli 60
-p1.pracuj(10)  # naliczy się 8*30 + 2 * 60 czyli 360
+p1.pracuj(10)  # naliczy się 8*30 + 2 * 60 + 300 czyli 660
 print(p1)  # Pracownik("Jan")
-print(p1.wyplata())  # 420
-p1._zarobki = 1_000_000
+print(p1.wyplata())  # 720
 print(p1.wyplata())  # 0
